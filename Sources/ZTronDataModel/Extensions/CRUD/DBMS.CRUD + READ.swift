1263a35139fc3e00068b29c70bec561cb7c9f42b
NickTheFreak97 <niccolo.dellarocca@gmail.com>
2024-11-25 14:04:08 +0100

    Attempt to disambiguate colorHex column

--- Sources/ZTronDataModel/Extensions/CRUD/DBMS.CRUD + READ.swift
+++ Sources/ZTronDataModel/Extensions/CRUD/DBMS.CRUD + READ.swift
@@ -242,7 +242,7 @@ extension DBMS.CRUD {
         )
         
         let boundingCircleExists = SQLite.Expression<String?>(
-            boundingCircle.colorHexColumn.template.droppingQuotes()
+            boundingCircle.table[boundingCircle.colorHexColumn].template.droppingQuotes()
         )
 
         
