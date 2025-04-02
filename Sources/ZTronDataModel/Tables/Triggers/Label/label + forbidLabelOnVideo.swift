2552402d22e9ee37a49215c692f8718abde05f66
= <niccolo.dellarocca@gmail.com>
2025-04-02 14:11:25 +0200

    Namespaced triggers

--- Sources/ZTronDataModel/Tables/Triggers/Label/label + forbidLabelOnVideo.swift
+++ Sources/ZTronDataModel/Tables/Triggers/Label/label + forbidLabelOnVideo.swift
@@ -9,12 +9,12 @@ extension Label {
             WHEN (
                 SELECT \(DomainModel.visualMedia.tableName).\(DomainModel.visualMedia.typeColumn.template) 
                 FROM \(DomainModel.visualMedia.tableName) JOIN \(DomainModel.label.tableName) ON
-                \(DomainModel.visualMedia.tableName).\(DomainModel.visualMedia.nameColumn.template) = \(DomainModel.boundingCircle.tableName).\(DomainModel.label.foreignKeys.imageColumn.template) AND
-                \(DomainModel.visualMedia.tableName).\(DomainModel.visualMedia.foreignKeys.galleryColumn.template) = \(DomainModel.boundingCircle.tableName).\(DomainModel.label.foreignKeys.galleryColumn.template) AND
-                \(DomainModel.visualMedia.tableName).\(DomainModel.visualMedia.foreignKeys.toolColumn.template) = \(DomainModel.boundingCircle.tableName).\(DomainModel.label.foreignKeys.toolColumn.template) AND
-                \(DomainModel.visualMedia.tableName).\(DomainModel.visualMedia.foreignKeys.tabColumn.template) = \(DomainModel.boundingCircle.tableName).\(DomainModel.label.foreignKeys.tabColumn.template) AND
-                \(DomainModel.visualMedia.tableName).\(DomainModel.visualMedia.foreignKeys.mapColumn.template) = \(DomainModel.boundingCircle.tableName).\(DomainModel.label.foreignKeys.mapColumn.template) AND
-                \(DomainModel.visualMedia.tableName).\(DomainModel.visualMedia.foreignKeys.gameColumn.template) = \(DomainModel.boundingCircle.tableName).\(DomainModel.label.foreignKeys.gameColumn.template)
+                \(DomainModel.visualMedia.tableName).\(DomainModel.visualMedia.nameColumn.template) = \(DomainModel.label.tableName).\(DomainModel.label.foreignKeys.imageColumn.template) AND
+                \(DomainModel.visualMedia.tableName).\(DomainModel.visualMedia.foreignKeys.galleryColumn.template) = \(DomainModel.label.tableName).\(DomainModel.label.foreignKeys.galleryColumn.template) AND
+                \(DomainModel.visualMedia.tableName).\(DomainModel.visualMedia.foreignKeys.toolColumn.template) = \(DomainModel.label.tableName).\(DomainModel.label.foreignKeys.toolColumn.template) AND
+                \(DomainModel.visualMedia.tableName).\(DomainModel.visualMedia.foreignKeys.tabColumn.template) = \(DomainModel.label.tableName).\(DomainModel.label.foreignKeys.tabColumn.template) AND
+                \(DomainModel.visualMedia.tableName).\(DomainModel.visualMedia.foreignKeys.mapColumn.template) = \(DomainModel.label.tableName).\(DomainModel.label.foreignKeys.mapColumn.template) AND
+                \(DomainModel.visualMedia.tableName).\(DomainModel.visualMedia.foreignKeys.gameColumn.template) = \(DomainModel.label.tableName).\(DomainModel.label.foreignKeys.gameColumn.template)
             ) <> 'image'
             BEGIN
                 SELECT RAISE(ABORT, 'cannot attach a label to a video');
--- Tests/ZTronDataModelTests/ZTronDataModelTests.swift
+++ Tests/ZTronDataModelTests/ZTronDataModelTests.swift
@@ -34,5 +34,35 @@ final class ZTronDataModelTests: XCTestCase {
             tool: "love the 80s",
             gallery: "master"
         )
+        
+        try DBMS.CRUD.insertIntoOutline(
+            for: connection,
+            resourceName: "outline",
+            colorHex: "#FF11AA",
+            isActive: true,
+            opacity: 1.0,
+            boundingBox: .init(origin: .zero, size: .init(width: 0.5, height: 0.5)),
+            game: "infinite warfare",
+            map: "spaceland",
+            tab: "music",
+            tool: "love the 80s",
+            gallery: "master",
+            image: "nunchaku step 1"
+        )
+        
+        try DBMS.CRUD.insertIntoBoundingCircle(
+            for: connection,
+            colorHex: "#FF1100",
+            isActive: true,
+            opacity: 1.0,
+            idleDiameter: nil,
+            normalizedCenter: .init(x: 0.5, y: 0.5),
+            game: "infinite warfare",
+            map: "spaceland",
+            tab: "music",
+            tool: "love the 80s",
+            gallery: "master",
+            image: "nunchaku step 1"
+        )
     }
 }
